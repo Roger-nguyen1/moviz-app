70eb059a625df00d8eb8c66018e3753d3563df49
Roger-nguyen1 <roger.nguyen@hotmail.fr>
2024-06-24 22:49:32 +0200

    changed style of card, scrollview added

--- app/movieDetails.tsx
+++ app/movieDetails.tsx
@@ -1,44 +1,64 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import { Card, Image } from "@rneui/themed";
 import { useRouter, useLocalSearchParams } from "expo-router";
 import formatDate from "@/utils/formatDate";
 
-interface Movie {
+type Movie = {
   id: number;
   title: string;
   poster_path: string;
   overview: string;
   release_date: string;
-}
+};
 
 const MovieDetails: React.FC = () => {
   const { movie } = useLocalSearchParams();
   const movieData: Movie = JSON.parse(movie as string);
 
   return (
-    <Card>
-      <Card.Title>{movieData.title}</Card.Title>
-      <Image
-        source={{
-          uri: `https://image.tmdb.org/t/p/w500${movieData.poster_path}`,
-        }}
-        style={styles.image}
-      />
-      <Text style={styles.text}>{movieData.overview}</Text>
-      <Text style={styles.text}>
-        Release Date: {formatDate(movieData.release_date)}
-      </Text>
-    </Card>
+    <ScrollView style={styles.mainContainer}>
+      <Card containerStyle={styles.cardContainer}>
+        <Card.Title style={styles.title}>{movieData.title}</Card.Title>
+        <Image
+          source={{
+            uri: `https://image.tmdb.org/t/p/w500${movieData.poster_path}`,
+          }}
+          style={styles.image}
+        />
+        <Text style={styles.text}>{movieData.overview}</Text>
+        <Text style={styles.text}>
+          Release Date: {formatDate(movieData.release_date)}
+        </Text>
+      </Card>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    backgroundColor: "#050251",
+    paddingTop: 22,
+  },
+  cardContainer: {
+    backgroundColor: "#050251",
+    paddingVertical: 20,
+    borderRadius: 8,
+    borderWidth: 0,
+  },
+  title: {
+    color: "#f3f6f4",
+    fontSize: 36,
+    fontWeight: "600",
+    fontFamily: "Montserrat_600SemiBold",
+  },
   text: {
+    color: "#f3f6f4",
+    fontFamily: "Montserrat_400Regular",
     marginBottom: 14,
   },
   image: {
-    height: 440,
+    height: 460,
     width: "100%",
     marginBottom: 14,
   },
