1d386b320956b1e6caffd9ced403f870eb1844e1
Roger-nguyen1 <roger.nguyen@hotmail.fr>
2024-06-24 22:47:41 +0200

    added expo-google-fonts with montserrat and pacifico

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,48 @@
+import { useCallback } from "react";
+import { View } from "react-native";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import {
+  useFonts as useMontserrat,
+  Montserrat_400Regular,
+  Montserrat_600SemiBold,
+} from "@expo-google-fonts/montserrat";
+import {
+  useFonts as usePacifico,
+  Pacifico_400Regular,
+} from "@expo-google-fonts/pacifico";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded] = useMontserrat({
+    Montserrat_400Regular,
+    Montserrat_600SemiBold,
+  });
+
+  const [pacificoLoaded] = usePacifico({
+    Pacifico_400Regular,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded && pacificoLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, pacificoLoaded]);
+
+  if (!fontsLoaded || !pacificoLoaded) {
+    return null;
+  }
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="moviesList" options={{ headerShown: false }} />
-      <Stack.Screen name="movieDetails" options={{ headerShown: false }} />
-    </Stack>
+    <View
+      style={{ flex: 1, backgroundColor: "#050251" }}
+      onLayout={onLayoutRootView}
+    >
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="moviesList" options={{ headerShown: false }} />
+        <Stack.Screen name="movieDetails" options={{ headerShown: false }} />
+      </Stack>
+    </View>
   );
 }
